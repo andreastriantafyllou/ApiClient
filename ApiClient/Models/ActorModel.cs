067661d982e0addf5ec3ab4b813de2f8dbbb34ba
Andreas Triantafyllou <aandreasam@gmail.com>
2018-07-29 16:21:10 +0300

    Create ActorModel. Birthday is string because format is only date.

---
+++ ApiClient/Models/ActorModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiClient.Models
+{
+    class ActorModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("birthday")]
+        public string Birthday { get; set; }
+    }
+}
