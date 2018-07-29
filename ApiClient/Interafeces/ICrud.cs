8c2eed26f427c3c16022c29d5e61faef59f404ca
Andreas Triantafyllou <aandreasam@gmail.com>
2018-07-29 13:29:39 +0300

    Create Crud interface with generic type.

---
+++ ApiClient/Interafeces/ICrud.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiClient.Interafeces
+{
+    interface ICrud<T>
+    {
+        string Create(T obj);
+        List<T> ReadAll ();
+        T ReadById(string id);
+        string Update(string key);
+        string Delete(string key);
+    }
+}
