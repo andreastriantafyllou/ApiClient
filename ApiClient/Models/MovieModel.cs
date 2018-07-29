be6d63a8058860035c73c4ee4108621c02e4e90d
Andreas Triantafyllou <aandreasam@gmail.com>
2018-07-29 20:18:37 +0300

    Create CRUD for movies.

---
+++ ApiClient/Models/MovieModel.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiClient.Models
+{
+    public class MovieModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("year")]
+        public int ProductionYear { get; set; }
+        [JsonProperty("actors")]
+        public List<int> ActorIds { get; set; }
+        [JsonProperty("director")]
+        public int DirectorIds { get; set; }
+    }
+}
---
+++ ApiClient/Modules/MoviesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApiClient.Interafeces;
+using ApiClient.Models;
+using Newtonsoft.Json;
+
+namespace ApiClient.Modules
+{
+    public class MoviesController : ICrud<MovieModel>
+    {
+        MoviesDBController db;
+        SharedCrudController crud;
+
+        public MoviesController()
+        {
+            db = new MoviesDBController();
+            crud = new SharedCrudController();
+        }
+
+        public string Create(MovieModel obj)
+        {
+            return crud.Create(obj, db.moviesDbScheme.MoviesPath);
+        }
+
+        public string Delete(int id)
+        {
+            return crud.Delete(id, db.moviesDbScheme.MoviesPath + id.ToString() + "/");
+        }
+
+        public List<MovieModel> ReadAll()
+        {
+            return JsonConvert.DeserializeObject<List<MovieModel>>(crud.ReadAll(db.moviesDbScheme.MoviesPath));
+        }
+
+        public MovieModel ReadById(int id)
+        {
+            return JsonConvert.DeserializeObject<MovieModel>(crud.ReadById(id, db.moviesDbScheme.MoviesPath + id.ToString() + "/"));
+        }
+
+        public string Update(int id, MovieModel newObj)
+        {
+            return crud.Update(id, newObj, db.moviesDbScheme.MoviesPath + id.ToString() + "/");
+        }
+    }
+}
