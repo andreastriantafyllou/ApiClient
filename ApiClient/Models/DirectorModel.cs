c95dfdb55926c985c592eac01cb997ea3f8010bc
Andreas Triantafyllou <aandreasam@gmail.com>
2018-07-29 18:57:26 +0300

    Create CRUD for Directors.

---
+++ ApiClient/Models/DirectorModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiClient.Models
+{
+    public class DirectorModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("birthday")]
+        public string Birthday { get; set; }
+    }
+}
---
+++ ApiClient/Modules/DirectorsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApiClient.Interafeces;
+using ApiClient.Models;
+using Newtonsoft.Json;
+
+namespace ApiClient.Modules
+{
+    public class DirectorsController : ICrud<DirectorModel>
+    {
+        MoviesDBController db;
+        SharedCrudController crud;
+
+        public DirectorsController  ()
+        {
+            db = new MoviesDBController();
+            crud = new SharedCrudController();
+        }
+
+        public string Create(DirectorModel obj)
+        {
+            return crud.Create(obj, db.moviesDbScheme.DirectorsPath);
+        }
+
+        public string Delete(int id)
+        {
+            return crud.Delete(id, db.moviesDbScheme.DirectorsPath + id.ToString() + "/");
+        }
+
+        public List<DirectorModel> ReadAll()
+        {
+            return JsonConvert.DeserializeObject<List<DirectorModel>>(crud.ReadAll(db.moviesDbScheme.DirectorsPath));
+        }
+
+        public DirectorModel ReadById(int id)
+        {
+            return JsonConvert.DeserializeObject<DirectorModel>(crud.ReadById(id, db.moviesDbScheme.DirectorsPath + id.ToString() + "/"));
+        }
+
+        public string Update(int id, DirectorModel newObj)
+        {
+            return crud.Update(id, newObj, db.moviesDbScheme.DirectorsPath + id.ToString() + "/");
+        }
+    }
+}
