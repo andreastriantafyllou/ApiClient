5b063d8810ee03a3471801488ec68ed87177fa0b
Andreas Triantafyllou <aandreasam@gmail.com>
2018-07-29 15:03:08 +0300

    Create MoviesDBScheme mobel.

---
+++ ApiClient/Models/MoviesDBScheme.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiClient.Models
+{
+    class MoviesDBScheme
+    {
+        [JsonProperty("actors")]
+        public string ActorsPath { get; set; }
+        [JsonProperty("directors")]
+        public string DirectorsPath { get; set; }
+        [JsonProperty("movies")]
+        public string MoviesPath { get; set; }
+    }
+}
